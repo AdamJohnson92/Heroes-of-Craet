04baf2c4efdf2cc26d8cdc58ae6122756aac92a3
AdamJohnson92 <127777953+AdamJohnson92@users.noreply.github.com>
2023-11-20 13:38:33 -0700

    Added text to preamble

--- src/components/Preamble.js
+++ src/components/Preamble.js
@@ -12,8 +12,9 @@ export default function Preamble() {
             </h2>
 
 
-            <h3> Untold Dangers Await</h3>
-            <p>Hello World!</p>
+            <h3 className= 'centered'> Untold Dangers Await </h3>
+            <p className='paragraph'>The land of Craet has a rich history full of heroes and their many deeds. Valorous knights, and daring rogues. Disciplined monks, and wise sorcerors. This land was built on their many virtues.</p> 
+            <p className='paragraph'>But Craet is once again being cast into shadow. Monsters are infecting the forests and plains, and threatening the peaceful citizens of the kingdom. Heroes must rise once again to slay the monsters where they stand. Will you be a Hero of Craet?</p> 
           
         </div>
     )
