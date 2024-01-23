64184a1fa3f5da3f606c15addcb0374affe3eeca
AdamJohnson92 <127777953+AdamJohnson92@users.noreply.github.com>
2024-01-23 13:44:00 -0700

    Added character avatars to hero selection buttons.

--- src/components/SelectCharBtns.js
+++ src/components/SelectCharBtns.js
@@ -7,6 +7,7 @@ export default function SelectCharBtns(props) {
         <>
         {props.characterRoster.map((char)=> (
             <button className="btn character-btn" id={char.name} onClick={props.handleChooseCharacter} key = {char.name}>{char.name} the {char.charClass}
+            <img id="character-img-div" src={char.img} ></img>
            </button>
         ))}
         </>
