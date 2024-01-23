653a78cad86cef057fb5e8e5c8a03e67a2e424e6
AdamJohnson92 <127777953+AdamJohnson92@users.noreply.github.com>
2024-01-23 14:10:45 -0700

    Fixed bug where image on button could not be clicked. Now image is above button.

--- src/components/SelectCharBtns.js
+++ src/components/SelectCharBtns.js
@@ -3,13 +3,17 @@ import React from "react"
 
 export default function SelectCharBtns(props) {
 
-    return(
+    return (
         <>
-        {props.characterRoster.map((char)=> (
-            <button className="btn character-btn" id={char.name} onClick={props.handleChooseCharacter} key = {char.name}>{char.name} the {char.charClass}
-            <img id="character-img-div" src={char.img} ></img>
-           </button>
-        ))}
+            {props.characterRoster.map((char) => (
+                <div id='character-selection-div'>
+                    <img id="character-img-div" src={char.img} ></img>
+                    <button className="btn character-btn" id={char.name} onClick={props.handleChooseCharacter} key={char.name}>{char.name} the {char.charClass}
+
+                    </button>
+                </div>
+
+            ))}
         </>
     )
 }
