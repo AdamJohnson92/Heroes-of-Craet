f8855eb5103b2435d6adbf7a454db8b90a772e01
AdamJohnson92 <127777953+AdamJohnson92@users.noreply.github.com>
2024-01-26 15:25:11 -0700

    Added text and styling to the shop screen and a button to link back to the character selection page

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useState, createContext } from "react";
+import { Link } from 'react-router-dom'
 
 
 export default function Shop() {
 
     return (
-        <div>
-            <h1>WELCOME TO THE SHOP. Coming Soon</h1>
+        <div className="centered">
+            <div className="container parchment centered">
+                <h2>WELCOME TO THE SHOP <br></br> Coming Soon!</h2>
+                <p className="centered">Here, you will be able to spend that hard earned gold on magical items to help you on your quest. Until then, continue fighting the good fight!</p>
+                <Link to='/Heroes-of-Craet/'>
+                    <button className='btn' id="to-town-btn"> Back to Character Selection</button>
+                </Link>
+            </div>
+
         </div>
     )
 
