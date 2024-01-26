3b5a57af389beb46f5997adbfce65a4955ef40eb
AdamJohnson92 <127777953+AdamJohnson92@users.noreply.github.com>
2024-01-26 16:02:01 -0700

    Fixed small styling bug on character selectino button in the shop

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -10,7 +10,7 @@ export default function Shop() {
                 <h2>WELCOME TO THE SHOP <br></br> Coming Soon!</h2>
                 <p className="centered">Here, you will be able to spend that hard earned gold on magical items to help you on your quest. Until then, continue fighting the good fight!</p>
                 <Link to='/Heroes-of-Craet/'>
-                    <button className='btn' id="to-town-btn"> Back to Character Selection</button>
+                    <button className='btn' id="to-town-btn"> Character Selection</button>
                 </Link>
             </div>
 
