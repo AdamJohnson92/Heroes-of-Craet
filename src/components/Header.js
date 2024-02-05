843cc8d617b58604687ea9373e15bab9ae36f2bd
AdamJohnson92 <127777953+AdamJohnson92@users.noreply.github.com>
2024-02-05 14:04:14 -0700

    Restyled header to remove grey background color, and added blue curved borders to create an underlined-effect on the page title

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,10 @@ function Header() {
         <header>
             <div className="outer-border">
                 <h1 id="page-title">Heroes of Craet</h1>
+                <div className="double-underline">
+                    <div className="single-underline"></div>
+                    <div className="single-underline"></div>
+                </div>
             </div>
         </header>
     )
