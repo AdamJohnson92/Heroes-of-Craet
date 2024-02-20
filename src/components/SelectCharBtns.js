23cdf7e6519dee608e08202d2d807d8399e01175
AdamJohnson92 <127777953+AdamJohnson92@users.noreply.github.com>
2024-02-20 14:35:34 -0700

    Added alt tags on character stat page image and character selection button icons

--- src/components/SelectCharBtns.js
+++ src/components/SelectCharBtns.js
@@ -7,7 +7,7 @@ export default function SelectCharBtns(props) {
         <>
             {props.characterRoster.map((char) => (
                 <div id='character-selection-div'>
-                    <img className="character-img-div" id={char.name === props.chosenCharacter.name ? 'currentChar' : 'notCurrentChar'}src={char.img} ></img>
+                    <img className="character-img-div" id={char.name === props.chosenCharacter.name ? 'currentChar' : 'notCurrentChar'}src={char.img} alt={char.staticAlt}></img>
                     <button className="btn character-btn" id={char.name} onClick={props.handleChooseCharacter} key={char.name}>{char.name} the {char.charClass}
 
                     </button>
